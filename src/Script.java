7c4366601bac506265e37da445985c98f34adde9
wayback-home <dudtj4329@gmail.com>
2021-12-18 11:34:01 +0900

    불필요한 내용 삭제, 최초 코드 작성

--- src/Script.java
+++ src/Script.java
@@ -1,8 +1,55 @@
+
 public class Script {
     // * 재정, 민심, 군사, 종교, 스크립트 문장, 대답1, 대답2 순
-    public void makeScript() {
-        setScript sentence1 = new setScript("plusBig", "plussmall", "minusBig", "minussmall", "테스트문장1", "테스트1", "테스트2");
+    private String Money[] = new String[100];
+    private String Person[] = new String[100];
+    private String Army[] = new String[100];
+    private String Religion[] = new String[100];
+    private String Sentence[] = new String[100];
+    private String Answer1[] = new String[100];
+    private String Answer2[] = new String[100];
 
-        setScript sentence2 = new setScript("plusBig", "plussmall", "minusBig", "minussmall", "테스트문장2", "테스트1", "테스트2");
+    // * set메서드
+    // * 배열형태로 저장, 호출시 int ID 이용
+    public void setScript() {
+        Money[0] = "none";
+        Person[0] = "none";
+        Army[0] = "none";
+        Religion[0] = "none";
+        Sentence[0] = "새로운 왕이 즉위하였습니다.";
+        Answer1[0] = "전하..!";
+        Answer2[0] = "아뢰옵니다!!";
+
     }
+
+    // * get메서드
+    // * setScript.java에서 호출시 이용
+    public String getMoney(int ID) {
+        return Money[ID];
+    }
+
+    public String getPerson(int ID) {
+        return Person[ID];
+    }
+
+    public String getArmy(int ID) {
+        return Army[ID];
+    }
+
+    public String getReligion(int ID) {
+        return Religion[ID];
+    }
+
+    public String getSentence(int ID) {
+        return Sentence[ID];
+    }
+
+    public String getAnswer1(int ID) {
+        return Answer1[ID];
+    }
+
+    public String getAnswer2(int ID) {
+        return Answer2[ID];
+    }
+
 }
--- src/getScript.java
+++ src/getScript.java
@@ -1,17 +1,17 @@
 import java.util.Random;
 
-public class setScript {
-    int money, person, army, religion; // * 순서대로 재정, 민심, 군사, 종교
-                                       // * 변동값을 결정하는 변수
-                                       // * (+)와 (-)부호로 증감 결정(작은변동 = 1~10, 큰 변동 11~20, 랜덤 이용)
-    String sentence; // * 스크립트 내용, 문장단위
-    String yes, no; // * 문장에 대한 대답, 2지선다로 "예, 아니요" 등의 내용
+public class getScript extends Script {
+    int money, person, army, religion;
+    /*
+     * 순서대로 재정, 민심, 군사, 종교
+     * 변동값을 결정하는 변수
+     * (+)와 (-)부호로 증감 결정(작은변동 = 1~10, 큰 변동 11~20, 랜덤 이용)
+     */
 
-    public setScript(String getMoney, String getPerson, String getArmy, String getReligion,
-            String getSentence, String getYes, String getNo) { // ! 반드시 모든 항목을 받도록 지정
-        Random getRandom = new Random();
+    Random getRandom = new Random();
 
-        switch (getMoney) {
+    public int returnMoney(String money) {
+        switch (money) {
             case "plusBig":
                 this.money = getRandom.nextInt(11);
                 break;
@@ -27,9 +27,19 @@ public class setScript {
             case "minusSmall":
                 this.money = -(getRandom.nextInt(11) + 10);
                 break;
-        }
 
-        switch (getPerson) {
+            case "none":
+                this.money = 0;
+                break;
+
+        }
+        return this.money;
+    }
+
+    public int returnPerson(String person) {
+        switch (person)
+
+        {
             case "plusBig":
                 this.person = getRandom.nextInt(11);
                 break;
@@ -45,9 +55,18 @@ public class setScript {
             case "minusSmall":
                 this.person = -(getRandom.nextInt(11) + 10);
                 break;
-        }
 
-        switch (getArmy) {
+            case "none":
+                this.person = 0;
+                break;
+        }
+        return this.person;
+    }
+
+    public int returnArmy(String army) {
+        switch (army)
+
+        {
             case "plusBig":
                 this.army = getRandom.nextInt(11);
                 break;
@@ -63,9 +82,16 @@ public class setScript {
             case "minusSmall":
                 this.army = -(getRandom.nextInt(11) + 10);
                 break;
-        }
 
-        switch (getReligion) {
+            case "none":
+                this.army = 0;
+                break;
+        }
+        return this.army;
+    }
+
+    public int returnReligion(String religion) {
+        switch (religion) {
             case "plusBig":
                 this.religion = getRandom.nextInt(11);
                 break;
@@ -81,10 +107,13 @@ public class setScript {
             case "minusSmall":
                 this.religion = -(getRandom.nextInt(11) + 10);
                 break;
-        }
 
-        this.sentence = getSentence;
-        this.yes = getYes;
-        this.no = getNo;
+            case "none":
+                this.religion = 0;
+                break;
+
+        }
+        return this.religion;
     }
+
 }
